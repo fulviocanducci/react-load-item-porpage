1e2a58f41bd07abccccff392d0246d27bdf76d5b
Fulvio <fulviocanducci@hotmail.com>
2019-08-03 20:31:44 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,86 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import axios from 'axios';
+import Pagination from 'react-js-pagination';
 
-function App() {
+const PokemonCard = (props) => {
+  return (    
+      <div className="col-sm-6 col-md-6">
+        <div className="thumbnail">          
+          <div className="caption">
+            <h3>{props.name}</h3>            
+          </div>
+        </div>
+      </div>    
+  );
+};
+
+const PokenRequestStatus = (props) => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={props.status==='done'?"alert alert-success":"alert alert-info"} role="alert">
+      {props.status}
     </div>
   );
+}
+
+class App extends Component {
+  constructor(){
+    super();
+    this.state = {
+      activePage: 1,      
+      data: {
+        count: 0,
+        next: '',
+        previous: '',
+        results:[]
+      },
+      status: 'ready',
+      counterPerPage: 6   
+    };
+    this.handlePageChange = this.handlePageChange.bind(this);
+  }
+  handlePageChange(pageNumber) {    
+    this.setState({activePage: pageNumber}, () => {
+      this.loadItems(pageNumber);
+    });
+    return true;
+  }
+  loadItems(page) {
+    const url = 'https://pokeapi.co/api/v2/pokemon/?offset=' + page + '&limit=6';
+    this.setState({status:'load'}, () => {
+      axios.get(url)
+        .then(response => {        
+          this.setState({data: response.data}, () => {
+            this.setState({status:'done'});
+          });        
+        });
+    });    
+  }
+  async componentDidMount(){
+    this.loadItems(this.state.activePage);
+  }
+  render() {
+    return (
+      <div className="container">
+        <div className="row">
+          {this.state.data.results.map((result,index) => (
+            <PokemonCard key={index} name={result.name}></PokemonCard>
+          ))}          
+        </div>
+        <div>
+          <PokenRequestStatus status={this.state.status}></PokenRequestStatus>
+        </div>
+        <div className="text-center">
+          <Pagination
+            activePage={this.state.activePage}
+            itemsCountPerPage={6}
+            totalItemsCount={this.state.data.count}
+            pageRangeDisplayed={5}
+            onChange={this.handlePageChange}
+          />
+        </div>
+      </div>
+    );
+  }  
 }
 
 export default App;
